02211bb368cc6bd8f732e90aa58dafbabaed5460
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-08-26 21:28:58 -0400

    Browser Router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+//STATE
+
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <h1>Header</h1>
+      
     </>
   );
 };
