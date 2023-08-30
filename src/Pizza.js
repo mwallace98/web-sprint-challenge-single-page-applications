dbda5c0befc83edb9e9389addb83c1778e59aed6
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-08-29 21:00:19 -0400

    name,special instructions and size state updating

--- src/Pizza.js
+++ src/Pizza.js
@@ -1,8 +1,64 @@
 import { useNavigate,Link } from 'react-router-dom'
+import React,{useState, useEffect} from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+
+const schema = yup.object().shape({
+    name: yup
+    .string()
+    .trim()
+    .required('Username is Required')
+    .min(2,"name must be at least 2 characters")
+})
+
+
+const initialValues = {
+    name:'',
+    size: '',
+    pepperoni: false,
+    mushrooms: false,
+    beef: false,
+    chicken: false,
+    specialInstructions: '',
+
+}
+const initialFormErrors = {
+    name:'',
+}
+
+//STATE, 
+// need to create state to hold the initial values and then some way to update them
+// create a slice of state that holds the values in the form oncee they are updated
+// on submit needs to upadte the new values into a slice of state
 export default function OrderForm() {
+
+
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState(initialFormErrors);
+    
+
+    // const submit = (evt) => {     
+    //     }
+    //     evt.preventDefault()
+    //     axios.post('https://reqres.in/api/orders',newPizza)
+    //     .then(res => {
+    //         console.log(res)
+    //     })
+    //     .catch(err => console.log(err))
+    
+    
+    const change = (evt => {
+       const {name,specialInstructions,value} = evt.target;
+       setFormValues({...formValues, [name]: value, [specialInstructions]: value})
+       
+    })
+    
+
+
     return (
         <div>
-            <h1> Order Form</h1>
+            <h1> Order Form</h1> 
+            <form  id='pizza-form'>
             <label>
                 <h4>Name</h4>
                 <input 
@@ -10,14 +66,14 @@ export default function OrderForm() {
                     type='text'
                     name='name'
                     placeholder='Enter your Name'
+                    onChange={change}
                 />
             </label>
-            <form id='pizza-form'>
                 <h4>Choose size</h4>
             <label> 
                 <select id='size-dropdown'>
-                <option value='size'>Select a Size </option>
-                <option value='small'>Small</option>
+                <option name='size'>Select a Size </option>
+                <option name='small'>Small</option>
                 <option value='medium'>Medium</option>
                 <option value='large'>Large</option>
                 </select>
@@ -55,8 +111,10 @@ export default function OrderForm() {
                     type='text'
                     name='special instructions'
                     placeholder='Enter Special Istructions'
+                    onChange={change}
                 />
-                </label>
+                </label >
+                <input type='submit' value='Add to Order'/>
             </div>
             </form>
         </div>
